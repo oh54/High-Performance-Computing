a7e3d21de6cf28e3b8216ae90e69272a2c289c0d
Oskar Hint <oskar.hint@gmail.com>
2017-01-05 11:36:44 +0100

    removed unnecessary comments

--- Project1/from-mikkel/submit.sh
+++ Project1/from-mikkel/submit.sh
@@ -3,14 +3,9 @@
 #PBS -q hpcintro
 cd $PBS_O_WORKDIR
 
-
 echo 'CPU info'
 lscpu
 
-#echo 'Running studio NKM 50 60 70'
-#./matmult_c.studio nkm 50 60 70
-
-#sizes=( 16 32 64 128 256 512 1024 2048 )
 sizes=( 16 23 32 45 64 90 128 181 256 362 512 724 1024 1448 2048 2896 4096 )
 
 for size in ${sizes[@]}
@@ -18,15 +13,4 @@ for size in ${sizes[@]}
   ./matmult_c.studio $perm $size $size $size
  done
 
-#for N in ${sizes[@]}
-# do 
-#  for K in ${sizes[@]}
-#   do
-#    for M in ${sizes[@]}
-#     do
-#      echo $M $N $K
-#      ./matmult_c.studio mnk $M $N $K
-#     done
-#   done
-# done
 
