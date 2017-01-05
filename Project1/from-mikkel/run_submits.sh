58ad98fb8b1f44027bd372263020abb03f62fb1e
Mikkel Jensen <mikkeljensen73@gmail.com>
2017-01-05 11:26:36 +0100

    Added the last 4 to the run_submits.sh

--- Project1/from-mikkel/run_submits.sh
+++ Project1/from-mikkel/run_submits.sh
@@ -4,3 +4,7 @@ make OPT='-g -fast' CXX=sunCC
 
 qsub -v perm='mkn' submit.sh -o mkn_o.txt -e mkn_e.txt
 qsub -v perm='nkm' submit.sh -o nkm_o.txt -e nkm_e.txt
+qsub -v perm='nmk' submit.sh -o nmk_o.txt -e nmk_e.txt
+qsub -v perm='mnk' submit.sh -o mnk_o.txt -e mnk_e.txt
+qsub -v perm='kmn' submit.sh -o kmn_o.txt -e kmn_e.txt
+qsub -v perm='knm' submit.sh -o knm_o.txt -e knm_e.txt
