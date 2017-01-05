fb032e5c52f887ea2d20e7d43e04585761d824ab
Mikkel Jensen <mikkeljensen73@gmail.com>
2017-01-05 13:14:22 +0100

    Edited for-profiling

---
+++ Project1/for-profiling/submit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#PBS -q hpcintro
+cd $PBS_O_WORKDIR
+
+echo 'CPU info'
+lscpu
+
+sizes=( 16 23 32 45 64 90 128 181 256 362 512 724 1024 1448 2048 2896 4096 )
+
+for size in ${sizes[@]}
+ do
+  ./matmult_c.studio $perm $size $size $size
+ done
+
+
