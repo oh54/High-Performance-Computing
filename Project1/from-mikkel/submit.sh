8aec51b7dd5b0d48fe6f473fbf146362ad4b5ec3
Mikkel Jensen <mikkeljensen73@gmail.com>
2017-01-05 09:47:29 +0100

    added sizes in submit.sh to get memory to double every step

--- Project1/from-mikkel/submit.sh
+++ Project1/from-mikkel/submit.sh
@@ -10,7 +10,8 @@ lscpu
 #echo 'Running studio NKM 50 60 70'
 #./matmult_c.studio nkm 50 60 70
 
-sizes=( 16 32 64 128 256 512 1024 2048 )
+#sizes=( 16 32 64 128 256 512 1024 2048 )
+sizes=( 16 23 32 45 64 90 128 181 256 362 512 724 1024 1448 2048 )
 
 for size in ${sizes[@]}
  do
