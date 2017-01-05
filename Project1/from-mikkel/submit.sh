e5e6ca1bfd9defebded45f483d9d6664b7b2c441
Oskar Hint <oskar.hint@gmail.com>
2017-01-05 14:49:23 +0100

    added PBS options

--- Project1/from-mikkel/submit.sh
+++ Project1/from-mikkel/submit.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 #PBS -q hpcintro
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=10:00
+
 cd $PBS_O_WORKDIR
+
+module load studio
 
 #echo 'CPU info'
 #lscpu
 
 sizes=( 16 23 32 45 64 90 128 181 256 362 512 724 1024 1448 2048 2896 4096 )
+
 
 for size in ${sizes[@]}
  do
