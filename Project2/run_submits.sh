301ffa88bfb5e53f8b26c9b11139156b365da2df
Oskar Hint <oskar.hint@gmail.com>
2017-01-12 17:01:44 +0100

    added numthreads print

--- Project2/run_submits.sh
+++ Project2/run_submits.sh
@@ -3,7 +3,10 @@ module load studio
 make clean_all
 make
 
+# sequential experiments, varying d and varying n
 #qsub submit-seqd.sh -o seqd_o.txt -e seqd_e.txt
 #qsub submit-seqn.sh -o seqn_o.txt -e seqn_e.txt
 
-qsub submit-parn.sh -o parn_o.txt -e parn_e.txt
+# parallel experiments, varying n and varying t
+#qsub submit-parn.sh -o parn_o.txt -e parn_e.txt
+qsub submit-part.sh -o part_o.txt -e part_e.txt
