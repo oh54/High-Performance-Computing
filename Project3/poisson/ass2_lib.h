f8ef8e26159cb79aa9e01b45a4684ef175d76d67
oskar <oskar.hint@gmail.com>
2017-01-18 20:14:49 +0100

    removed gauss seidel stuff

--- Project3/poisson/ass2_lib.h
+++ Project3/poisson/ass2_lib.h
@@ -2,6 +2,3 @@
 
 void jacobi_seq(double ** u, double ** uo, double ** f, int N, double delta2);
 
-void gauss_seidel(double ** u, double ** f, int N, double delta2);
-
-void jacobi_omp(double ** u, double ** uo, double ** f, int N, double delta2);
