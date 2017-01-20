215eae44b649a8cd29291804a2e53f9d6191e74d
oskar <oskar.hint@gmail.com>
2017-01-20 23:51:03 +0100

    remove old stuff

--- Project3/poisson/ass2_lib.h
+++ Project3/poisson/ass2_lib.h
@@ -7,6 +7,3 @@ __global__ void jacobi_single_kernel(double * d_u, double * d_uo, double * d_f, 
 
 __global__ void jacobi_multi_kernel(double * d0_u, double * d0_uo, double * d0_f, int N, double delta2);
 
-__global__ void update_uo_multi_kernel(double * d0_u, double * d0_uo, int N);
-
-__global__ void update_uo_multi_kernel1(double * d1_u, double * d1_uo, int N);
